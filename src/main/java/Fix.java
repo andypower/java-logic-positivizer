bd5edbad1bd494f039f39e5e6d80453b125fcace
Danny van Bruggen <hexagonaal@gmail.com>
2017-05-04 22:16:14 +0200

    Also fix nonsense cast of "this"

--- src/main/java/Fix.java
+++ src/main/java/Fix.java
@@ -1,6 +1,7 @@
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.expr.BinaryExpr;
-import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.ImportDeclaration;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.expr.*;
 import com.github.javaparser.ast.stmt.IfStmt;
 import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
@@ -9,6 +10,7 @@ import com.github.javaparser.utils.CodeGenerationUtils;
 import com.github.javaparser.utils.SourceRoot;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Fix {
     public static void main(String[] args) throws IOException {
@@ -19,9 +21,9 @@ public class Fix {
             @Override
             public Visitable visit(IfStmt n, Void arg) {
                 Expression condExpr = n.getCondition();
-                if(condExpr instanceof BinaryExpr){
+                if (condExpr instanceof BinaryExpr) {
                     BinaryExpr cond = (BinaryExpr) condExpr;
-                    if(cond.getOperator() == BinaryExpr.Operator.NOT_EQUALS && n.getElseStmt().isPresent()){
+                    if (cond.getOperator() == BinaryExpr.Operator.NOT_EQUALS && n.getElseStmt().isPresent()) {
                         Statement thenStmt = n.getThenStmt().clone();
                         Statement elseStmt = n.getElseStmt().get().clone();
                         n.setThenStmt(elseStmt);
@@ -30,6 +32,21 @@ public class Fix {
                     }
                 }
                 return super.visit(n, arg);
+            }
+
+            @Override
+            public Visitable visit(FieldAccessExpr n, Void arg) {
+                Optional<Visitable> simpleName = n.getScope()
+                        .filter(s -> s instanceof EnclosedExpr)
+                        .map(s -> (EnclosedExpr) s)
+                        .flatMap(s -> s.getInner()
+                                .filter(i -> i instanceof CastExpr)
+                                .map(i -> (CastExpr) i)
+                                .filter(i -> i.getExpression() instanceof ThisExpr)
+                                .map(t -> new NameExpr(n.getName()))
+                        );
+
+                return simpleName.orElse(super.visit(n, arg));
             }
         }, null);
 
