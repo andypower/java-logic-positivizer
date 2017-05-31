994da9dd77e0af5c6b4dc65ab2a953e81eb57cfd
Danny van Bruggen <hexagonaal@gmail.com>
2017-05-31 21:17:41 +0200

    Adjust to article: https://matozoid.github.io/2017/05/06/inverting-ifs.html

--- src/main/java/Fix.java
+++ src/main/java/Fix.java
@@ -1,6 +1,4 @@
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.ImportDeclaration;
-import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.expr.*;
 import com.github.javaparser.ast.stmt.IfStmt;
 import com.github.javaparser.ast.stmt.Statement;
@@ -14,16 +12,24 @@ import java.util.Optional;
 
 public class Fix {
     public static void main(String[] args) throws IOException {
+        // SourceRoot is a tool that read and writes Java files from packages on a certain root directory.
+        // In this case the root directory is found by taking the root from the current Maven module,
+        // with src/main/resources appended.
         SourceRoot sourceRoot = new SourceRoot(CodeGenerationUtils.mavenModuleRoot(Fix.class).resolve("src/main/resources"));
+        // Our sample is in the root of this directory, so no package name.
         CompilationUnit cu = sourceRoot.parse("", "Blabla.java");
 
         cu.accept(new ModifierVisitor<Void>() {
             @Override
             public Visitable visit(IfStmt n, Void arg) {
+                // I figured out what to get and what to cast simply by looking at the AST in the debugger! 
                 Expression condExpr = n.getCondition();
                 if (condExpr instanceof BinaryExpr) {
                     BinaryExpr cond = (BinaryExpr) condExpr;
                     if (cond.getOperator() == BinaryExpr.Operator.NOT_EQUALS && n.getElseStmt().isPresent()) {
+                        /* It's a good idea to clone nodes that you move around.
+                            JavaParser (or you) might get confused about who their parent is!
+                        */
                         Statement thenStmt = n.getThenStmt().clone();
                         Statement elseStmt = n.getElseStmt().get().clone();
                         n.setThenStmt(elseStmt);
@@ -50,6 +56,7 @@ public class Fix {
             }
         }, null);
 
+        // This saves back the file we read with the changes we made. Easy!
         sourceRoot.saveAll();
     }
 }
